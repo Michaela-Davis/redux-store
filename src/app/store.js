905d8a4b906aa6b7bd70967c9a8474cbd16cb88c
Michaela Davis <michaela.delight@gmail.com>
2025-01-04 13:37:18 -0800

    :sparkles: Add Slice Reducers to the Store

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from '../features/counter/counterSlice'
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    // by adding a reducer parameter, we tell the store to use this slice reducer function to handle all updates to that state.
+    counter: counterReducer, 
+  },
 })
